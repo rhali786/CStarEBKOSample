c633f525e97a5aad99130bc287767b20067bcdf7
Rasheed Ali <rhali786@gmail.com>
2018-01-09 14:34:10 -0500

    add cpp to debug

---
+++ Debug/test.cpp
@@ -0,0 +1,31 @@
+
+    private boolean isAppetiteRetrieved(List<RiskPosition> riskPositions) {
+        if (riskPosition != null && !riskPositions.isEmpty() && !isOnlyAddedCodes(riskPositions)) {
+//TODO: THIS IS NEVER TRUE, where is the bug?
+            return true;
+        }
+        return false;
+    }
+
+
+int main()
+{
+    struct tm * tmp;
+    time_t s;
+
+    for (; ;) {
+        s = time(NULL);
+        tmp = localtime(&t);
+
+        printf("%d:%0d:%d\n", temp ->tm_hour, tmp ->tm_min, tmp ->tm_sec);
+//TODO: always get an error. Where is the bug? 
+        Sleep(1000);
+
+        system("cls");
+
+    }
+
+    return 0;
+}
+
+
